9ef083a48d1cf9ddf1a26c3e5ceda549fc98befa
Bharat Bhushan <bbhushan@uoguelph.ca>
2022-04-25 19:36:31 -0400

    Added list printing functionality

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
     char choice {};
 
+    std::vector<int> nums;
+
     do
     {
-        std::cout << "P - Print numbers" << std::endl;
+        std::cout << "\nP - Print numbers" << std::endl;
         std::cout << "A - Add a number" << std::endl;
         std::cout << "M - Display mean of the numbers" << std::endl;
         std::cout << "S - Display the smallest number" << std::endl;
@@ -16,7 +19,50 @@ int main()
         std::cout << "\nEnter your choice: ";
         std::cin >> choice;
 
-    }while(choice )
+        if (choice == 'P' || choice == 'p')
+        {
+        	if (nums.size() > 0)
+        	{
+        		std::cout << "[ ";
+        		for (int num: nums)
+        		{
+        			std::cout << num << " ";
+        		}
+        		std::cout << "]\n";
+
+        	}
+        	else
+        	{
+        		std::cout << "[] - the list is empty.\n";
+        	}
+        }
+        else if(choice == 'A' || choice == 'a')
+        {
+        	
+        }
+        else if(choice == 'M' || choice == 'm')
+        {
+        	
+        }
+        else if(choice == 'S' || choice == 's')
+        {
+        	
+        }
+        else if(choice == 'L' || choice == 'l')
+        {
+        	
+        }
+        else if(choice == 'Q' || choice == 'q')
+        {
+        	std::cout << "Goodbye.";
+        }
+        else
+        {
+        	std::cout << "Unknown selection, please try again.";
+        }
+
+
+    }while(choice != 'Q' && choice != 'q');
     
     return 0;
 }
